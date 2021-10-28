485592253f1441a3eedc2499470a90f84ec9e167
Leonardo <leonardo@LAPTOP-2S3MJ3JD>
2021-10-28 00:39:09 -0300

    Create architecture repositories for calcs to corrections

--- correcao.solo/correcao.solo/Form1.cs
+++ correcao.solo/correcao.solo/Form1.cs
@@ -16,5 +16,10 @@ namespace correcao.solo
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
